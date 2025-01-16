5d052f6f34ec4844c9e5c65c8e148a85951bfa0e
Luis <levalladares>
2025-01-16 00:49:35 +0000

    Taller21

---
+++ src/pages/pokemonPage.vue
@@ -0,0 +1,19 @@
+<template>
+    <h2>Adivina el pokemon de la imagen</h2>
+    <pokemonImagen />
+    <pokemonOpciones />
+</template>
+
+<script>
+import pokemonImagen from '../components/pokemonImagen.vue'
+import pokemonOpciones from '@/components/pokemonOpciones.vue'  // otra forma de importar
+export default {
+    components: {
+        pokemonImagen,
+        pokemonOpciones
+    }
+
+}
+</script>
+
+<style></style>
