73c336fda0664244e1a5a93239c44deb8148f01e
Luis <levalladares>
2025-02-15 00:48:11 +0000

    Taller36

--- src/client/PersonaCliente.js
+++ src/client/PersonaCliente.js
@@ -1,11 +1,32 @@
 import axios from "axios";
 const obtenerPorId = async (id) => {
-    //http://localhost:8080/matriculaAPI/v1.1/personas/
-   const data = await axios.get(`http://localhost:8080/matriculaAPI/v1.1/personas/${id}`).then(r => r.data);
+    //http://localhost:8081/matriculaAPI/v1.1/personas/
+   const data = await axios.get(`http://localhost:8081/matriculaAPI/v1.1/personas/${id}`).then(r => r.data);
    console.log(data);
    return data;
 }
+const insertar = async (body) => {   
+    axios.post('http://localhost:8081/matriculaAPI/v1.1/personas/', body).then(r => r.data);
+}
+
+const actualizar = async (body) => {   
+    axios.post('http://localhost:8081/matriculaAPI/v1.1/personas/{id}', body).then(r => r.data);
+}
+const actualizarParcial = async (body) => {   
+    axios.patch('http://localhost:8081/matriculaAPI/v1.1/personas/{id}', body).then(r => r.data);
+}
+const eliminar = async (body) => {   
+    axios.delete('http://localhost:8081/matriculaAPI/v1.1/personas/{id}', body).then(r => r.data);
+}
+
+
+
 //metodo fachada
 export const obtenerPorIdFachada = async (id) => {
+    console.log("obtenerPorIdFachada");
     return await obtenerPorId(id);
+}
+
+export const insertarFachada = async (body) => {   
+    return await insertar(body);
 }
--- src/components/FormularioComponente.vue
+++ src/components/FormularioComponente.vue
@@ -1,40 +1,122 @@
 <template>
-  <h1>FORMULARIO PERSONA</h1>
   <div class="container">
-    <h1>Formulario Persona</h1>
     <div class="formulario">
-      <p type="Nombre:"><input type="text" /></p>
-      <p type="Apellido:"><input type="text" /></p>
-      <p type="Fecha Nacimiento:"><input type="text" /></p>
+      <h2>Formulario Persona</h2>
+
+      <div class="forma">
+        <label>{{ id }}</label>
+        <input type="text" v-model="datos.id" />
+      </div>
+      <div class="forma">
+        <label>{{ nombre }}</label>
+        <input type="text" v-model="datos.nombre" />
+      </div>
+      <div class="forma">
+        <label>{{ apellido }}</label>
+        <input type="text" v-model="datos.apellido" />
+      </div>
+      <div class="forma">
+        <label>{{ fecha }}</label>
+        <input type="date" v-model="datos.fecha" />
+      </div>
+      <div class="button-center">
+        <button v-on:click="buscar()">Consultar</button>
+        <button v-on:click="guardar()">Consultar</button>
+      </div>
     </div>
-    <button>Agregar</button>
   </div>
 </template>
 
 <script>
-import { obtenerPorIdFachada } from '../client/PersonaCliente'
+import { obtenerPorIdFachada, insertarFachada } from '@/client/PersonaCliente';
 export default {
-  mounted
+  mounted() {
+    console.log("Antes de llamar al API")
+    obtenerPorIdFachada(3)
+  },
+  data() {
+    return {
+      id: "ID:",
+      nombre: "Nombre:",
+      apellido: "Apellido:",
+      fecha: "Fecha:",
+
+      datos: {
+        id: "",
+        nombre: "",
+        apellido: "",
+        fecha: "",
+      },
+    };
+  },
+  methods: {
+    async buscar() {
+      const data = await obtenerPorIdFachada(this.datos.id)
+      this.datos.nombre = data.nombre
+      this.datos.apellido = data.apellido
+      this.datos.fecha = data.fecha
+    },
+    async guardar() {
+      const bodyPersona={
+        nombre: this.datos.nombre,
+        apellido: this.datos.apellido,
+        fecha: this.datos.fecha
+      }
+      await insertarFachada(bodyPersona)
+    }
 }
+};
 </script>
 
 <style>
 .container {
-  display: grid;
+  display: flex;
   justify-content: center;
   align-items: center;
 }
 
 .formulario {
-  display: grid;
-  flex-direction: center;
-  border: solid 1px black;
-  padding: 10px;
+  width: 300px;
+  padding: 20px;
+  border: 4px solid black;
+  color: black;
+  border-radius: 30px;
+  text-align: left;
+}
+
+.forma {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 10px;
+}
+
+label {
+  margin-bottom: 10px;
+}
+
+input {
+  padding: 5px;
+  border: 1px solid green;
   border-radius: 5px;
 }
 
-p::before {
-  content: attr(type);
-  width: 100px;
+.button-center {
+  display: flex;
+  justify-content: center;
+}
+
+button {
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+  background-color: green;
+  color: white;
+  cursor: pointer;
+  justify-content: center;
+  align-items: center;
+}
+
+button:hover {
+  background-color: darkgreen;
 }
 </style>
